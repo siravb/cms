341debc7ed8462d3f84a31d7cb3a9796cdc30fc8
siravb <sir_avb@mail.ru>
2019-06-21 22:24:56 +0600

    second

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Tag, Category, Comment
+
+
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+class Category(models.Model):
+    """Модель категорий статей"""
+    name = models.CharField("Название", max_length=150)
+    slug = models.SlugField("url", max_length=160)
+    active = models.BooleanField("Опубликовать", default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Tag(models.Model):
+    """Модель тегов к статьям"""
+    name = models.CharField("Название", max_length=50)
+    slug = models.SlugField("url", max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+class Post(models.Model):
+    """Модель статей"""
+    title = models.CharField("Заголовок", max_length=150)
+    sub_title = models.CharField("Подзаголовок", max_length=150)
+    mini_text = models.TextField("Превью статьи",max_length=500)
+    text = models.TextField("Текст статьи")
+    created = models.DateTimeField("Дата создания", auto_now_add=True)
+    publish_date = models.DateTimeField("Дата публикации", default=timezone.now)
+    active = models.BooleanField("Опубликовать", default=True)
+    sort = models.PositiveIntegerField("Порядок", default=0, unique=True)
+    view = models.PositiveIntegerField("Просмотры", default=0)
+    tags = models.ManyToManyField(Tag, verbose_name="Теги")
+    slug = models.SlugField("url", max_length=100)
+    category = models.ForeignKey(Category, verbose_name="Категория",on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+
+class Comment(models.Model):
+    """Модель комментариев к статьям"""
+    post = models.ForeignKey(
+        Post,
+        verbose_name="Статья",
+        on_delete=models.CASCADE
+    )
+    text = models.TextField("Комментарий")
+    created = models.DateTimeField("Дата написания", auto_now_add=True)
+    moderation = models.BooleanField("Разрешено к публикации", default=False)
+
+
+    def __str__(self):
+        return "{}".format(self.post.title)
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
